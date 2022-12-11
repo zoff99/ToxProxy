628a4c9558efffbe77e37840161b42f8363ded3e
zoff99 <zoff99@users.noreply.github.com>
2022-12-11 13:16:48 +0100

    add config

---
+++ src/push_server_config.h
@@ -0,0 +1,10 @@
+#ifndef C_PUSH_SERVER_CONFIG_H
+#define C_PUSH_SERVER_CONFIG_H
+
+#define PUSH__DST_PORT 888
+#define PUSH__DST_HOST "127.0.0.1"
+#define PUSH__MAXDATASIZE 200
+
+#define HTTP_PUSH__DST_URL "https://tox.zoff.xyz/toxfcm/fcm.php?id="
+
+#endif // C_PUSH_SERVER_CONFIG_H
