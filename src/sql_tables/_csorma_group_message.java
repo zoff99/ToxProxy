24ee761ebc9255f65f8528b81ba81bc890219838
zoff99 <zoff99@users.noreply.github.com>
2024-05-08 20:11:13 +0200

    add columns

--- src/sql_tables/_csorma_group_message.java
+++ src/sql_tables/_csorma_group_message.java
@@ -8,13 +8,24 @@ public class group_message
     public String groupid;
     @Column
     public String peerpubkey;
+
     @Column
     public String datahex;
+
+    @Column
+    public String wrappeddatahex;
+
     @Column
     public int message_id;
 
     @Column
     public int timstamp_recv;
+
+    @Column
+    public String message_hashid;
+
+    @Column
+    public String message_sync_hashid;
 
     @Column
     public int mtype;
