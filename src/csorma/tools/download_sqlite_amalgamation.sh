fcf7fb642ac14f567562b8a65da98843113dd889
zoff99 <zoff99@users.noreply.github.com>
2024-06-10 15:55:53 +0200

    update csorma

--- src/csorma/tools/download_sqlite_amalgamation.sh
+++ src/csorma/tools/download_sqlite_amalgamation.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-amalgamation_url="https://sqlite.org/2024/sqlite-amalgamation-3450300.zip"
+_SQLITE_VERSION_="3460000"
+
+amalgamation_url="https://sqlite.org/2024/sqlite-amalgamation-""$_SQLITE_VERSION_"".zip"
 
 
 _HOME2_=$(dirname $0)
@@ -11,7 +13,8 @@ export _HOME_
 basedir="$_HOME_""/../"
 cd "$basedir"
 
-wget --continue "$amalgamation_url" -O amalgamation.zip
+rm -f amalgamation.zip
+wget "$amalgamation_url" -O amalgamation.zip
 mkdir "$basedir""/sqlite/"
 cd "$basedir""/sqlite/"
 unzip -j -o "$basedir""/"amalgamation.zip
